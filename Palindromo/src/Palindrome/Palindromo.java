51fa07872ca1cdf63643d50c05d4709b159702b7
jorge <jorge@192.168.0.20>
2022-08-30 07:45:11 +0200

    done

--- Palindromo/src/Palindrome/Palindromo.java
+++ Palindromo/src/Palindrome/Palindromo.java
@@ -1,37 +1,28 @@
 package Palindrome;
-import java.util.*;
-import java.io.UnsupportedEncodingException;
 
+import java.util.Scanner;
 
 public class Palindromo {
 		
-	public static void main(String[] args) throws UnsupportedEncodingException {
+	public static void main(String[] args)  {
 		    
-		Scanner sc = new Scanner (System.in);
-	    System.out.println("Introduce una palabra: ");
-	    String new_text = sc.nextLine().toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una palabra: ");
+		
+		String letter = sc.nextLine().toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
 				.replace("ú","u").replace(" ","").replace(".","").replace(",","");
-	    
-	    String reverse = "";
-	        
-	        for (int i = new_text.length() - 1; i >=0; --i) {
-	
-	        	reverse = reverse + new_text.charAt(i);
-	             
-	          }
-	    
-	        if (new_text.equals(reverse)) {
-	       
-	         System.out.println( new_text + " es palindromo\n");
-	        }
-	        else {
-	         
-	          System.out.println(new_text + " no es palindromo\n");
-	        }
-	        
-	        
-	      
-	       
-         }
-	
+		
+		String reverse = "";
+		
+		for(int i=letter.length() - 1; i >=0; i--) {
+			
+			reverse = reverse + letter.charAt(i);
+		}
+		
+			if(letter.equals(reverse)) {
+				System.out.println(letter + "es palindromo\n");
+			}else {
+				System.out.println(letter + "no es palindromo");
+			}	
+		}
 }
